dc2ac0d2343195d06051734787141577fc6fb8e0
Юрий Михайлов <yuri_2002_q@mail.ru>
2021-12-23 17:46:30 +0400

    Mikhailov_Lab1_ysl

---
+++ ISEbd21_Mikhailov_YS/WindowsFormsTeplovoz/DirectionTube.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsTeplovoz
+{
+        public enum DirectionTube
+        {
+            One = 1,
+            Two = 2,
+            Three = 3
+        }
+ }
